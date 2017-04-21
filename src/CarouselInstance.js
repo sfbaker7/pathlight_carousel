112140207c8b35d1206304e1c69c608d705f97e0
Stuart Baker <stuartbaker@Stuarts-MacBook-Pro.local>
2017-04-21 19:24:07 -0400

    final commit

--- src/CarouselInstance.js
+++ src/CarouselInstance.js
@@ -8,21 +8,29 @@ class CarouselInstance extends React.Component {
     constructor() {
         super();
         this.state = {};
+
     }
 
     componentWillMount() {
-      let url = 'https://source.unsplash.com/random/900x500';
+        let url = 'https://api.unsplash.com/photos';
+        Request.get(url).then((response) => {
+            this.setState({
+                image: response.urls.raw
+            })
+        })
 
     }
 
     render() {
+        let image = this.state.image
         return (
-          <Carousel>
+          <Carousel
+          >
             <Carousel.Item>
               <img width="100%" height="100%" alt="900x500" src="https://source.unsplash.com/random/900x500"/>
             </Carousel.Item>
             <Carousel.Item>
-              <img width="100%" height="100%" alt="900x500" src="https://source.unsplash.com/random/900x500"/>
+              <img width="100%" height="100%" alt="900x500" src="https://source.unsplash.com/category/nature/900x500"/>
             </Carousel.Item>
             <Carousel.Item>
               <img width="100%" height="100%" alt="900x500" src="https://source.unsplash.com/random/900x500"/>
